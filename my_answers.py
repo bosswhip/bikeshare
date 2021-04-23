46a2e594fe3a93adf956e3fee9ba343ff17f9bec
bosswhip <83085012+bosswhip@users.noreply.github.com>
2021-04-23 15:13:16 -0600

    Update my_answers.py

--- my_answers.py
+++ my_answers.py
@@ -20,7 +20,7 @@ class NeuralNetwork(object):
         #
         # Note: in Python, you can define a function with a lambda expression,
         # as shown below.
-        self.activation_function = lambda x : 1 / (1 + np.exp(-x))  # Replace 0 with your sigmoid calculation.
+        self.activation_function = lambda x : 1.0 / (1.0 + np.exp(-x))  # Replace 0 with your sigmoid calculation.
         
         ### If the lambda code above is not something you're familiar with,
         # You can uncomment out the following three lines and put your 
@@ -141,9 +141,9 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += delta_weights_h_o * self.lr # update hidden-to-output weights with gradient descent step
+        self.weights_hidden_to_output += delta_weights_h_o * self.lr / n_records
 #         print("weight h to o", self.weights_hidden_to_output)
-        self.weights_input_to_hidden += delta_weights_i_h * self.lr # update input-to-hidden weights with gradient descent step
+        self.weights_input_to_hidden += delta_weights_i_h * self.lr / n_records # update input-to-hidden weights with gradient descent step
 #         print("weight i to h", self.weights_input_to_hidden)
 
     def run(self, features):
@@ -179,5 +179,5 @@ class NeuralNetwork(object):
 ##########################################################
 iterations = 200
 learning_rate = 0.1
-hidden_nodes = 20
+hidden_nodes = 50
 output_nodes = 1
